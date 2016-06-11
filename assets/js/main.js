7ff68261a207a39188ee0c25d851cdf53c447910
manozbiswas <mkbiitdu@gmail.com>
2016-06-11 09:35:11 -0700

    Show to cleint

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
-/**
- * Created by debnath on 08/06/2016.
- */
+$(document).ready(function(){
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1000) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+});
